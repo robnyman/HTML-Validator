6329f498c9831ae2dffe321f93be030ba145b604
Robert Nyman <robnyman@gmail.com>
2010-04-07 01:02:06 +0200

    Minor end of URL check fix for images, latest version of jQuery

--- google-chrome/js/validate.js
+++ google-chrome/js/validate.js
@@ -41,7 +41,7 @@ var htmlvalidator = function () {
 			
 		chrome.extension.onRequest.addListener(receiveRequest);
 		
-		if (!(/acid3.acidtests.org|\.(png|gif|jpe?g)/.test(location.href))) {
+		if (!(/acid3.acidtests.org|\.(png|gif|jpe?g)$/.test(location.href))) {
 			loading = $('<div id="html-validator-loading"><img src="' + chrome.extension.getURL("images/loading.gif") + ' " />Validating...</div>').appendTo(body);
 		
 			messagePresentation = $('<div id="html-validator-message"><span id="html-validation-message-close">X</span><div id="html-validator-message-content"></div></div>').appendTo(body);
